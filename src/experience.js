cf14ea42fb96804e69da8b8a85cd4544f46ef0bc
Himer65 <112268377+Himer65@users.noreply.github.com>
2023-10-13 16:31:03 +0700

    Update experience.js

--- src/experience.js
+++ src/experience.js
@@ -1,7 +1,5 @@
 let exp = "\
 <b>\
-2023.10.12 - Программирование контролеров на базе TwinCat.<br></br>\
-2023.10.06 - Изучение языка ST (Structured Text)<br></br>\
 2023.09.18 - Изучение HTML/CSS/JS и написание этой <a target='_blank' href='https://github.com/Himer65/himer65.github.io'>визитки-сайта.</a><br></br>\
 Опыт работы с Linux и командной строкой.<br></br>\
 2022.10.05 - Начало работы с фреймворком <a target='_blank' href='https://github.com/Himer65/pytorch-test'>PyTorch.</a><br></br>\
