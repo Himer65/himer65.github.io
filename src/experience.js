07ba77673e8d3baab8cf8529a9958e5b046c63c9
Himer65 <nurikmeskov@gmail.com>
2023-10-16 17:21:17 +0700

    YES!!!

--- src/experience.js
+++ src/experience.js
@@ -1,12 +1,13 @@
-let exp = "\
-<b>\
-2023.09.18 - Изучение HTML/CSS/JS и написание этой <a target='_blank' href='https://github.com/Himer65/himer65.github.io'>визитки-сайта.</a><br></br>\
-Опыт работы с Linux и командной строкой.<br></br>\
-2022.10.05 - Начало работы с фреймворком <a target='_blank' href='https://github.com/Himer65/pytorch-test'>PyTorch.</a><br></br>\
-2021 год - Изучение библеотек/фреймворков: Numpy/Pandas/Scikit Learn/Matplotlib.<br></br>\
-2021 год - Начало изучения языка Python, в частности Искуственного Интелекта.<br></br>\
-</b>\
-"
+let exp = `
+<b>
+2023.10.16 - Node.js, Express, Vue.<br></br>
+2023.09.18 - Изучение HTML/CSS/JS и написание этой <a target='_blank' href='https://github.com/Himer65/himer65.github.io'>визитки-сайта.</a><br></br>
+Опыт работы с Linux и командной строкой.<br></br>
+2022.10.05 - Начало работы с фреймворком <a target='_blank' href='https://github.com/Himer65/pytorch-test'>PyTorch.</a><br></br>
+2021 год - Изучение библеотек/фреймворков: Numpy, Pandas, ScikitLearn, Matplotlib<br></br>
+2021 год - Начало изучения языка Python, в частности Искуственного Интелекта.<br></br>
+</b>
+`
 
 let app = new Vue({
     el: '#experience',
