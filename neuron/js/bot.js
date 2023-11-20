1102e27b7d12c0d2bdf0a76efda121d3c93c57a8
Himer65 <nurikmeskov@gmail.com>
2023-11-20 12:06:31 +0700

    add Bot V4: yes or no?

--- neuron/js/bot.js
+++ neuron/js/bot.js
@@ -1,77 +1,53 @@
-const StrToArray = function(str) {
-    let [fen, tour] = str.split(" ").slice(0, 2);
-    fen = fen.replace(/\//gi, "")
-        .replace(/1/gi, ".")
-        .replace(/2/gi, "..")
-        .replace(/3/gi, "...")
-        .replace(/4/gi, "....")
-        .replace(/5/gi, ".....")
-        .replace(/6/gi, "......")
-        .replace(/7/gi, ".......")
-        .replace(/8/gi, "........");
-    let hot = new Float32Array(770).fill(0.0);
+const _piece = {
+    "r": 0, "q": 3,
+    "n": 1, "k": 4,
+    "b": 2, "p": 5,
+};
 
-    for (let idx = 0; idx < fen.length; idx++) {
-        switch (fen[idx]) {
-            case "r":
-                hot[idx*12 +  0] = 1.0;
-                continue;
-            case "n":
-                hot[idx*12 +  1] = 1.0;
-                continue;
-            case "b": 
-                hot[idx*12 +  2] = 1.0;
-                continue;
-            case "q":
-                hot[idx*12 +  3] = 1.0;
-                continue;
-            case "k":
-                hot[idx*12 +  4] = 1.0;
-                continue;
-            case "p":
-                hot[idx*12 +  5] = 1.0;
-                continue;
-            case "R":
-                hot[idx*12 +  6] = 1.0;
-                continue;
-            case "N":
-                hot[idx*12 +  7] = 1.0;
-                continue;
-            case "B":
-                hot[idx*12 +  8] = 1.0;
-                continue;
-            case "Q":
-                hot[idx*12 +  9] = 1.0;
-                continue;
-            case "K":
-                hot[idx*12 + 10] = 1.0;
-                continue;
-            case "P":
-                hot[idx*12 + 11] = 1.0;
-                continue;
-            case ".":
-                continue;
+
+const StrToArray = function(fen) {
+    let hot = new Float32Array(385).fill(0);
+
+    for (let idx = 0, hidx = 0; idx < fen.length; idx++, hidx += 6) {
+        if (fen[idx] === " ") {
+            let tour = fen[idx + 1] === "b" ? 1: -1;
+            hot[384] = tour;
+            break;
+        } else if (fen[idx] === "/") {
+            hidx -= 6;
+            continue;
+        } else if (!Object.keys(_piece).includes(fen[idx].toLowerCase())) {
+            hidx += parseInt(fen[idx]) * 6 - 6;
+            continue;
         }
+
+        let P = !(fen[idx].toUpperCase() == fen[idx]) ? 1: -1;
+        hot[hidx + _piece[fen[idx].toLowerCase()]] = P;
     }
 
-    if (tour === "b") {
-        hot[768] = 1.0;
-    } else {
-        hot[769] = 1.0;
-    }
-
-    const x = new onnx.Tensor(hot, "float32", [1, 770]);
+    const x = new onnx.Tensor(hot, "float32", [1, 385]);
     return x;
 };
 
+let _cache = {};
+
 const Bot = async function(fen) {
-    const inp = StrToArray(fen);
-    let out = await _bot.run([inp]);
-    return out.values().next().value.data[0];
+    let [part1, part2] = fen.split(" ").slice(0, 2);
+    fen = part1 + " " + part2;
+
+    if (Object.keys(_cache).includes(fen)) {
+        return _cache[fen];
+    } else {
+        const inp = StrToArray(fen);
+        let out = await _bot.run([inp]);
+        out = out.values().next().value.data[0];
+        _cache[fen] = out;
+        return out;
+    }
 };
 
 
 const _bot = new onnx.InferenceSession();
-_bot.loadModel("./js/ChessBot.onnx").then(() => {
+_bot.loadModel("./js/PlanktonBot.onnx").then(() => {
     console.log("Bot download");
 });
