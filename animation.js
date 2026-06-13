4ce60f63881911ccc0ea4b7f0fce1ab4e601ad32
Himer65 <nurikmeskov@gmail.com>
2026-06-13 11:28:02 +0300

    refactoring

--- animation.js
+++ animation.js
@@ -4,10 +4,10 @@ let boll = document.querySelector(".boll");
 const winH = window.innerHeight - 30
 const winW = window.innerWidth - 30
 
-let bollX = Math.floor(30);
+let bollX = winH / 2;
 let speedX = Math.random() * 4 - 2;
 
-let bollY = Math.floor(30);
+let bollY = winW / 2;
 let speedY = Math.random() * 4 - 2;
 
 
@@ -21,4 +21,4 @@ const boll_animation = function () {
     bollX += speedX; bollY += speedY;
     boll.style = `top: ${bollX}px; left: ${bollY}px`;
 };
-setInterval(boll_animation, 15);
+setInterval(boll_animation, 10);
