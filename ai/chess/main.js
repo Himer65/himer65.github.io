d255e1a7408f5aec1c524a8c89751b1f59325c6f
Himer65 <nurikmeskov@gmail.com>
2026-06-26 18:29:08 +0300

    add 'my ai' page

--- ai/chess/main.js
+++ ai/chess/main.js
@@ -1,11 +1,59 @@
-function main(){
-    ctx.drawImage(board, 0, 0);
+function square(square, piece) {
+    let moves = game.moves({square: square, verbose: true});
+
+    if (moves.length === 0) return;
+
+    for (let i = 0; i < moves.length; i++) {
+        let $square = $("#board .square-" + moves[i].to);
+        $square.css("background", "#2cff367b");
+        
+        if ($square.hasClass('black-3c85d')) {
+            $square.css("background", "#125d157b");
+        }
+    }
 }
 
+function removeSquare(square, piece) {
+    $("#board .square-55d63").css("background", "");
+}
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const board = new Image();
-board.src = "./image/board.png";
+function onDragStart(source, piece, position, orientation) {
+  if ((piece.search(/^w/) === -1) || game.game_over()) {
+    return false;
+  }
+}
 
-window.setInterval(main, 30);
+function onDrop (source, target) {
+    removeSquare();
+
+    let move = game.move({
+        from: source,
+        to: target,
+        promotion:"q",
+    });
+
+    if (move === null) return "snapback";
+}
+
+function onSnapEnd () {
+    board.position(game.fen());
+}
+
+let config = {
+    position: "start",
+    draggable: true, 
+    dropOffBoard: "snapback",
+    pieceTheme: "./image/{piece}.png",
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd,
+    onMouseoverSquare: square,
+    onMouseoutSquare: removeSquare,
+};
+
+const board = Chessboard("board", config);
+const game = new Chess();
+$("#startButton").on("click", function startButton() {
+    board.start();
+    game.reset();
+});
